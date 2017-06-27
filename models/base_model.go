e21137e669e7d094618bed7ebbffb7b31c79e537
Hubert Mine <hubert.mine@chauffeur-prive.com>
2017-06-27 16:24:13 +0200

    TECH Add base models

---
+++ models/base_model.go
@@ -0,0 +1,77 @@
+package models
+
+import (
+	"errors"
+
+	"github.com/sirupsen/logrus"
+	"github.com/transcovo/go-chpr-logger"
+	mgo "gopkg.in/mgo.v2"
+)
+
+/*
+ErrGenericDB is the error returned when an unknown mongo error happens.
+*/
+var ErrGenericDB = errors.New("DB Error")
+
+/*
+BaseModelInterface interface  defines the methods all models must implement.
+*/
+type BaseModelInterface interface {
+	EnsureIndexes(bool)
+	GetCollection() *mgo.Collection
+}
+
+/*
+BaseModel struct declares the required methods to implement the BaseModelInterface interface.
+*/
+type BaseModel struct {
+	CollectionName string
+	DB             *mgo.Database
+	Indexes        []*mgo.Index
+}
+
+/*
+EnsureIndexes creates the model indexes in the database.
+*/
+func (model *BaseModel) EnsureIndexes(background bool) {
+	for _, index := range model.Indexes {
+		index.Background = background
+	}
+	ensureIndexes(model.GetCollection(), model.Indexes)
+}
+
+/*
+GetCollection returns the model collection.
+*/
+func (model *BaseModel) GetCollection() *mgo.Collection {
+	return model.DB.C(model.CollectionName)
+}
+
+type indexInserter interface {
+	EnsureIndex(mgo.Index) error
+}
+
+/*
+ensureIndexes sets in mongo a list of indexes in a given collection.
+*/
+func ensureIndexes(dbCollection indexInserter, indexes []*mgo.Index) {
+	for _, index := range indexes {
+		err := dbCollection.EnsureIndex(*index)
+		if err != nil {
+			logger.WithFields(logrus.Fields{
+				"err":   err,
+				"index": index,
+			}).Error("[models.ensureIndexes] Error creating index")
+			panic(err)
+		}
+	}
+}
+
+/*
+SetupIndexes sets in mongo all the indexes for all the given models.
+*/
+func SetupIndexes(models []BaseModelInterface, background bool) {
+	for _, model := range models {
+		model.EnsureIndexes(background)
+	}
+}
---
+++ models/base_model_test.go
@@ -0,0 +1,72 @@
+package models
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/transcovo/mgo-helper/mongo"
+	"gopkg.in/mgo.v2"
+)
+
+type testDocument struct {
+	Key   string `bson:"key"`
+	Value int    `bson:"value"`
+}
+
+/*
+TestBaseModel_EnsureIndexes tests that the indexes are correctly created
+*/
+func TestBaseModel_EnsureIndexes(t *testing.T) {
+	config := mongo.Configuration{
+		URL:           "mongodb://localhost:27017/some-test-db",
+		UseSSL:        false,
+		SSLCert:       []byte{},
+		PingFrequency: 100,
+	}
+
+	db, teardown := mongo.InitMongoFromConfig(config)
+	defer teardown()
+
+	// initialize the collection..
+	db.C("some-collection").Insert(&testDocument{})
+
+	model := &BaseModel{
+		DB:             db,
+		CollectionName: "some-collection",
+		Indexes: []*mgo.Index{
+			{
+				Unique: true,
+				Name:   "test_1",
+				Key:    []string{"first_key"},
+			},
+		},
+	}
+
+	SetupIndexes([]BaseModelInterface{model}, false)
+
+	indexes, err := db.C("some-collection").Indexes()
+	assert.Nil(t, err)
+	assert.Equal(t, []mgo.Index{
+		{Key: []string{"_id"}, Name: "_id_"},
+		{Key: []string{"first_key"}, Name: "test_1", Unique: true},
+	}, indexes)
+}
+
+type mockIndexInserter struct{}
+
+func (mock *mockIndexInserter) EnsureIndex(mgo.Index) error {
+	return errors.New("some random error")
+}
+
+/*
+TestEnsureIndexes_Error tests that if it fails during index insertions, it panics
+*/
+func TestEnsureIndexes_Error(t *testing.T) {
+	failingInserter := &mockIndexInserter{}
+	assert.Panics(t, func() {
+		ensureIndexes(failingInserter, []*mgo.Index{
+			{Key: []string{"some key"}},
+		})
+	})
+}
